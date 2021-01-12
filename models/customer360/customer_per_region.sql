c69db4f988bc690b2be32e85bf460dbd1f22cb3f
Tamas Foldi <tfoldi@starschema.net>
2021-01-12 21:39:38 +0000

    fix this and that

--- models/customer360/customer_per_region.sql
+++ models/customer360/customer_per_region.sql
@@ -1,4 +1,4 @@
-select count(*) region_count, r_name 
+select sum(*) region_count, r_name 
 from {{ref('customer_per_nation')}} inner join {{source('tpch_raw','region')}}
 on (n_regionkey = r_regionkey)
 group by 2
