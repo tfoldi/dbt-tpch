1438e89d39e00584e8629f6ba290e17ea68dcb08
Tamas Foldi <tfoldi@starschema.net>
2021-01-12 15:50:11 +0000

    implement region as well

--- models/customer360/customer_per_nation.sql
+++ models/customer360/customer_per_nation.sql
@@ -1,3 +1,3 @@
-select count(*) nation_count, n_name, n_regionid 
-from {{source('tpch-raw','customer')}} inner join {{source('tpch-raw','nation')}}
-group by 2,3,4
+select count(*) nation_count, n_name, n_regionkey 
+from {{source('tpch_raw','customer')}} inner join {{source('tpch_raw','nation')}}
+group by 2,3
