34177d1e071e71834376d504bbc0cd775b9f1926
Tamas Foldi <tfoldi@starschema.net>
2021-01-13 09:56:00 +0000

    add snapshot

--- models/customer360/customer_per_region.sql
+++ models/customer360/customer_per_region.sql
@@ -1,4 +1,4 @@
-select sum(*) region_count, r_name 
+select sum(nation_count) region_count, r_name 
 from {{ref('customer_per_nation')}} inner join {{source('tpch_raw','region')}}
 on (n_regionkey = r_regionkey)
 group by 2
